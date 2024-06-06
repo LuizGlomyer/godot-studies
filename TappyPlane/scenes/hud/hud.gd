8839cbbe91ff48448ef8510a6e0862508fec733c
Luiz Glomyer <glomyerjunior@hotmail.com>
2024-06-05 22:22:33 -0400

    feat: Tappy Plane game

---
+++ TappyPlane/scenes/hud/hud.gd
@@ -0,0 +1,11 @@
+extends Control
+
+@onready var label = $MarginContainer/Label
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	SignalManager.on_score_updated.connect(on_score_updated)
+
+
+func on_score_updated() -> void:
+	label.text = str(ScoreManager.get_score())
