1eb33236ee065677e16cdd99f8e12e7306e5a3a8
Luiz Glomyer <glomyerjunior@hotmail.com>
2024-08-26 00:14:41 -0400

    feat: 3d games shaders

---
+++ GodotShaders/spatial_shaders_starter_project/scripts/spatial_shaders.gd
@@ -0,0 +1,13 @@
+extends Node3D
+
+@onready var sphere_example: MeshInstance3D = $SphereExample
+
+
+func _process(_delta):
+	if Input.is_action_just_pressed("quit"):
+		get_tree().quit()
+	
+	if Input.is_action_just_pressed("set_color"):
+		var sphere_material = sphere_example.get_active_material(0)
+		var random_color = Vector3(randf(), randf(), randf())
+		sphere_material.set_shader_parameter("sphere_color", random_color)
